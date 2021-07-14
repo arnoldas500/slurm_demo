070f113af9e777623f146d26b1c6a0fef9d560e9
arnoldas kurbanovas <akurbanovas@albany.edu>
2021-07-14 11:20:45 -0400

    Create slurm_test.sh

---
+++ slurm_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=GPUTest
+#SBATCH -p gpu
+#SBATCH --nodes=1
+#SBATCH -n 1
+#SBATCH --mem-per-cpu=1000
+#SBATCH --time=00:05:00
+#SBATCH --gres=gpu:1
+
+docker run --runtime=nvidia --rm tensorflow/tensorflow:latest-gpu nvidia-smi
